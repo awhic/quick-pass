520df341b1b01cbaefd90712e93a9dc3885d2fa0
alex <alexh1199@hotmail.com>
2021-11-13 16:03:18 -0500

    option to include or exclude special characters

--- QuikPass.java
+++ QuikPass.java
@@ -3,15 +3,30 @@ import java.util.Scanner;
 
 public class QuikPass {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         int length = setLength();
+        String specialChars;
 
+        try {
+            specialChars = setSpecChar();
+        } catch (Exception e) {
+
+            specialChars = "!@#$%^&*.";
+            System.out.println((char)27 + "[33m" + "Input Invalid. Defaulting to include special characters...");
+            Thread.sleep(3250L);
+        }
+        
         clearScreen();
-        System.out.println(generatePass(length));
+        Thread.sleep(500L);
+        String output = generatePass(length, specialChars).toString();
+
+        System.out.println("");
+        System.out.println((char)27 + "[37m" + output);
     }
 
     private static int setLength() {
+
         String askLength = "Password Length: ";
         String warnLength = "Invalid password length. Please enter a valid two-digit number.";
 
@@ -24,24 +39,43 @@ public class QuikPass {
             while (!lengthInq.hasNextInt()) {
                 System.out.println((char)27 + "[31m" + warnLength);
                 System.out.println((char)27 + "[37m" + askLength);
-                lengthInq.next(); // this is important!
+                lengthInq.next();
             }
             length = lengthInq.nextInt();
 
-            if (length <= 0) {
+            if (length <= 0 && length > 100) {
                 System.out.println((char)27 + "[31m" + warnLength);
             }
-        } while (length <= 0);
-        
-        lengthInq.close();
+        } while (length <= 0 && length >= 100);
+
         return length;
     }
 
-    private static char[] generatePass(int length) {
+    private static String setSpecChar() throws Exception {
+
+        Scanner specCharInq = new Scanner(System.in);
+        String specCharValue;
+        String specChar;
+
+        System.out.println("Do you want to include special characters? (y/n):");
+        specCharValue = specCharInq.nextLine();
+
+        if (specCharValue.equals("Y") || specCharValue.equals("y")) {
+            specChar = "!@#$%^&*.";
+        } else if (specCharValue.equals("N") || specCharValue.equals("n")) {
+            specChar = "";
+        } else {
+            specChar = "";
+            throw new Exception("Invalid Input");
+        }
+        return specChar;
+    }
+
+    private static char[] generatePass(int length, String specialCharacters) {
 
         String upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         String lowerCase = "abcdefghijklmnopqrstuvwxyz";
-        String specialChars = "!@#$%^&*";
+        String specialChars = specialCharacters;
         String numbers = "1234567890";
 
         String allTypesConcat = upperCase + lowerCase + specialChars + numbers;
@@ -51,7 +85,9 @@ public class QuikPass {
 
         password[0] = upperCase.charAt(rand.nextInt(upperCase.length()));
         password[1] = lowerCase.charAt(rand.nextInt(lowerCase.length()));
-        password[2] = specialChars.charAt(rand.nextInt(specialChars.length()));
+        if (!specialCharacters.equals("")) {
+            password[2] = specialChars.charAt(rand.nextInt(specialChars.length()));
+        }
         password[3] = numbers.charAt(rand.nextInt(numbers.length()));
 
         for(int i = 4; i < length; i++) {
@@ -60,9 +96,8 @@ public class QuikPass {
         return password;
     }
 
-    public static void clearScreen() {  
+    private static void clearScreen() {  
         System.out.print("\033[H\033[2J");  
-        System.out.flush();  
+        System.out.flush();
     }
-    
 }
